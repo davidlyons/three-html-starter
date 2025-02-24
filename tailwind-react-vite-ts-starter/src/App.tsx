9dddc8f95bd60321b4a45658644722538bb5abbe
David Lyons <david.scott.lyons@gmail.com>
2025-02-23 17:44:06 -0800

    update deps

--- tailwind-react-vite-ts-starter/src/App.tsx
+++ tailwind-react-vite-ts-starter/src/App.tsx
@@ -1,9 +1,13 @@
+import { PiGlobeHemisphereWestLight } from 'react-icons/pi'
+
 function App() {
   return (
     <>
       <div className="py-14">
         <div className="container">
-          <h1 className="text-4xl">Hello world</h1>
+          <h1 className="text-4xl">
+            Hello world <PiGlobeHemisphereWestLight />
+          </h1>
         </div>
       </div>
     </>
